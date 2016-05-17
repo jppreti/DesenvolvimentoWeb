db0e4860e74a388af98a7515656c7603cd43c7e4
jppreti <jppreti@gmail.com>
2016-05-17 14:16:59 -0400

    Correções textuais apenas.

--- web/src/main/java/unidade2/ServletBasico.java
+++ web/src/main/java/unidade2/ServletBasico.java
@@ -8,28 +8,23 @@ import java.io.IOException;
 @WebServlet
 public class ServletBasico implements Servlet {
 
-	@Override
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("MŽtodo init executado!");
+		System.out.println("Mï¿½todo init executado!");
 	}
 
-	@Override
 	public void service(ServletRequest req, ServletResponse res)
 			throws ServletException, IOException {
-		System.out.println("MŽtodo service executado!");
+		System.out.println("Mï¿½todo service executado!");
 	}
 
-	@Override
 	public void destroy() {
-		System.out.println("MŽtodo destroy executado!");
+		System.out.println("Mï¿½todo destroy executado!");
 	}
 
-	@Override
 	public ServletConfig getServletConfig() {
 		return null;
 	}
 
-	@Override
 	public String getServletInfo() {
 		return null;
 	}
